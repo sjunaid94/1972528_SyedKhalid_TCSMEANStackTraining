501e77d69e8c71491a6d905796b6366a8de96917
Syed <skhali31@uic.edu>
2021-03-17 16:51:45 -0500

    added css and upadted .js

--- Simply Blogging/simpleBlogging.js
+++ Simply Blogging/simpleBlogging.js
@@ -9,10 +9,7 @@ function addBlog(){
     console.log(articleObj);
     articleInfo.push(articleObj)
     storeArticle();
-    
-    // document.getElementById("titleInfo").innerHTML=title;
-    // document.getElementById("descInfo").innerHTML=desc;
-    // document.getElementById("imageInfo").src=imageInfo;
+
 }
 
 function storeArticle(){
@@ -27,37 +24,46 @@ function resetData(){
     document.getElementById("imageId").value="";
     var obj = JSON.parse(sessionStorage.getItem("blogObj"));
     insertNewRecord(obj);
+    articleInfo.pop();
 
 }
 
 
-
 function insertNewRecord(data){
 
-    
     for(i =0; i<data.length;i++){
         
-        var x = data[i].title;
-        var y = data[i].desc;
-        var z = data[i].imageId;
+        var title = data[i].title;
+        var desc = data[i].desc;
+        var img = data[i].imageInfo;
+
+        var containerDiv = document.createElement("div");
+        containerDiv.id = "create";
+        containerDiv.className="container"
+        const currentDiv = document.getElementById("lastDiv");
+        document.body.insertBefore(containerDiv, currentDiv);
+        
         
         var newDiv = document.createElement("div"); 
-        var newContent = document.createTextNode(x); 
-        newDiv.appendChild(newContent);  
-        var currentDiv = document.getElementById("titleInfo"); 
-        document.body.insertBefore(newDiv, currentDiv);
-        
-        var newDiv_1 = document.createElement("div_1"); 
-        var newContent_1 = document.createTextNode(y); 
-        newDiv_1.appendChild(newContent_1);  
-        var currentDiv_1 = document.getElementById("descInfo"); 
-        document.body.insertBefore(newDiv_1, currentDiv_1);
+        newDiv.className = "titleInfo";
+        newDiv.innerHTML = title;
+        document.getElementById("create").appendChild(newDiv);
 
-        var newDiv_2 = document.createElement("div_2"); 
-        var newContent_1 = document.createTextNode(z); 
-        newDiv_2.appendChild(newContent_1);  
-        var currentDiv_2 = document.getElementById("imageInfo"); 
-        document.body.insertBefore(newDiv_2, currentDiv_2);
+        var newDiv_1 = document.createElement("div");
+        newDiv_1.className = "descInfo"; 
+        newDiv_1.innerHTML = desc;
+        document.getElementById("create").appendChild(newDiv_1);
+
+        var newDiv_2 = document.createElement("img"); 
+        newDiv_2.className = "imageInfo";
+        newDiv_2.src = img;
+        document.getElementById("create").appendChild(newDiv_2);
+
+        var newdiv_3 = document.createElement("hr");
+        newdiv_3.className = "lineBreak";
+        document.getElementById("create").appendChild(newdiv_3);
+
+
         
     }
 }
