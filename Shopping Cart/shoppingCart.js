0f171c8af7019b97ea96eb596aa335327fc14854
Syed <skhali31@uic.edu>
2021-03-22 15:58:20 -0500

    Shopping Cart Project

---
+++ Shopping Cart/shoppingCart.js
@@ -0,0 +1,26 @@
+var cartItems = [];
+function addProduct(itemName, itemPrice) {
+    var cartobj = { productName: itemName, productPrice: itemPrice };
+    console.log(cartobj);
+    cartItems.push(cartobj);
+    console.log(cartItems.length);
+    document.getElementById("myOrder").innerHTML = JSON.stringify(cartItems.length);
+    sessionStorage.setItem("checkoutItems", JSON.stringify(cartItems));
+}
+function insertRecord() {
+    var obj = JSON.parse(sessionStorage.getItem("checkoutItems"));
+    var table = document.getElementById("itemsList");
+    var body = table.getElementsByTagName("tbody")[0];
+    var totalPrice = 0;
+    for (var i = 0; i < obj.length; i++) {
+        var x = obj[i].productName;
+        var y = obj[i].productPrice;
+        totalPrice += parseInt(y, 10);
+        var newRow = body.insertRow();
+        var cell1 = newRow.insertCell(0);
+        cell1.innerHTML = x;
+        var cell2 = newRow.insertCell(1);
+        cell2.innerHTML = y;
+    }
+    document.getElementById("totalPrice").innerHTML = "$" + JSON.stringify(totalPrice);
+}
---
+++ Shopping Cart/shoppingCart.ts
@@ -0,0 +1,41 @@
+let cartItems:Array<object> = [];
+interface CartItems{
+    productName: string;
+    productPrice: string;
+}
+function addProduct(itemName:string,itemPrice:string):void{
+
+    let cartobj:CartItems={productName:itemName,productPrice:itemPrice};
+    console.log(cartobj);
+    cartItems.push(cartobj);
+    console.log(cartItems.length);
+    document.getElementById("myOrder").innerHTML= JSON.stringify(cartItems.length);
+
+    sessionStorage.setItem("checkoutItems",JSON.stringify(cartItems));
+
+
+}
+
+function insertRecord(){
+    let obj = JSON.parse(sessionStorage.getItem("checkoutItems"));
+
+    let table = document.getElementById("itemsList");
+    let body = table.getElementsByTagName("tbody")[0];
+    let totalPrice = 0;
+
+
+    for(let i =0; i<obj.length;i++){
+        
+        let x = obj[i].productName;
+        let y = obj[i].productPrice;
+        totalPrice += parseInt(y,10);
+        let newRow = body.insertRow();
+        let cell1 = newRow.insertCell(0);  
+        cell1.innerHTML= x;                
+        let cell2 = newRow.insertCell(1); 
+        cell2.innerHTML=y;         
+       
+    }
+    document.getElementById("totalPrice").innerHTML = "$" + JSON.stringify(totalPrice);
+
+}
